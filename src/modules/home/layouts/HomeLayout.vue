eb9682d5158846e07d781d524b37d09ed1dc4947
Sebastian Ahumada <cbasmx@gmail.com>
2022-06-10 21:03:14 -0400

    fix routes prod

--- src/modules/home/layouts/HomeLayout.vue
+++ src/modules/home/layouts/HomeLayout.vue
@@ -14,7 +14,7 @@
                 <div class="modal-content">
                 <div class="modal-header">
                     <!-- <h5 class="modal-title" id="exampleModalLabel">Búsqueda:</h5> -->
-                    <input type="text" class="form-control me-2" v-model="busquedaComunaRegion" @keyup="buscarComunaRegion" @touchend="buscarComunaRegion" placeholder="Buscar Comuna o Región" />
+                    <input type="text" class="form-control me-2" v-model="busquedaComunaRegion" @keyup="buscarComunaRegion" @click="buscarComunaRegion" placeholder="Buscar Comuna o Región" />
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
